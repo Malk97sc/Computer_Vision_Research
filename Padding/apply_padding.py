795681dbe044a6435ec75df5db7c06a4c6a93008
Malk97sc <malaksanchez09@gmail.com>
2025-08-29 21:50:46 -0500

    Padding Function

--- Padding/apply_padding.py
+++ Padding/apply_padding.py
@@ -4,11 +4,11 @@ def padding(img, kernel):
     k_h, k_w = kernel.shape[:2]
     pad_h, pad_w = k_h // 2, k_w // 2
 
-    if len(img.shape) == 2:  # Grayscale
+    if len(img.shape) == 2:  #grayscale
         height, width = img.shape
         out = np.zeros((height + 2*pad_h, width + 2*pad_w), dtype=img.dtype)
         out[pad_h:pad_h+height, pad_w:pad_w+width] = img
-    elif len(img.shape) == 3:  # Color
+    elif len(img.shape) == 3:  #color
         height, width, channels = img.shape
         out = np.zeros((height + 2*pad_h, width + 2*pad_w, channels), dtype=img.dtype)
         out[pad_h:pad_h+height, pad_w:pad_w+width, :] = img
